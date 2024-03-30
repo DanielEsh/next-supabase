dfd4f54cf21955269adbad655a30456ad1b97b17
Daniel <daniilterawars@gmail.com>
2024-03-30 16:50:52 +0300

    feat(supabase): login

--- src/app/unauthenticated/page.tsx
+++ src/app/unauthenticated/page.tsx
@@ -2,19 +2,15 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function Home() {
+export default async function Unauthenticated() {
   const supabase = createServerComponentClient<Database>({ cookies });
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  if (!session) {
-    redirect("/unauthenticated");
+  if (session) {
+    redirect("/");
   }
 
-  return (
-    <>
-      <h1>Hello, {session.user.email}</h1>
-    </>
-  );
+  return <p>Please sign in to see todos!</p>;
 }
