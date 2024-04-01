e0e9855959aae3f5b6770511c844b0eafe073e9f
Daniel <daniilterawars@gmail.com>
2024-04-01 22:05:04 +0300

    feat(supabase): file storage

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -8,15 +8,65 @@ export const Users = () => {
     const [users, setUsers] = useState([])
 
     const getUsers = async () => {
-        const { data, error } = await supabase
-            .from('user')
-            .select(`
-                *,
-                user_group(*)
-  `);
-        setUsers(data)
+        try {
+            // Получаем информацию о пользователе
+            const { data: userData, error: userError } = await supabase
+                .from('user')
+                .select('id, first_name, username')
+                .eq('id', '05cbc7f7-5abd-4159-81c1-d6433be326cb')
+                .single();
 
-        console.log('DATA', data)
+            if (userError) {
+                throw userError;
+            }
+
+            // Получаем группы пользователя
+            const { data: userGroups, error: groupsError } = await supabase
+                .from('user_group')
+                .select('group_id')
+                .eq('user_id', '05cbc7f7-5abd-4159-81c1-d6433be326cb');
+
+            if (groupsError) {
+                throw groupsError;
+            }
+
+            // Получаем разрешения для каждой группы
+            const groupIds = userGroups.map(group => group.group_id);
+            const { data: groupPermissions, error: permissionsError } = await supabase
+                .from('group_permission')
+                .select('group_id, permission_id')
+                .in('group_id', groupIds);
+
+            if (permissionsError) {
+                throw permissionsError;
+            }
+
+            // Получаем информацию о разрешениях
+            const permissionIds = groupPermissions.map(permission => permission.permission_id);
+            const { data: permissionsData, error: permissionsDataError } = await supabase
+                .from('permission')
+                .select('name')
+                .in('id', permissionIds);
+
+            if (permissionsDataError) {
+                throw permissionsDataError;
+            }
+
+            // Формируем и возвращаем результат
+            const result = {
+                user: userData,
+                groups: userGroups,
+                permissions: permissionsData
+            };
+
+            console.log('RESULT', result)
+
+            return result;
+        } catch (error) {
+            console.error('Error fetching user groups and permissions:', error.message);
+            return null;
+        }
+
     }
 
     useEffect(() => {
