166d57d81c5291d5a1690eb703b441218c91e38e
Daniel <daniilterawars@gmail.com>
2024-04-06 19:27:50 +0300

    feat(backend): supabase

--- mono/my-turborepo/apps/backend/src/app.controller.ts
+++ mono/my-turborepo/apps/backend/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createClient } from '@supabase/supabase-js';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  supabase: any;
+
+  constructor(private readonly appService: AppService) {
+    this.supabase = createClient(
+      'https://hbnabwxbfbtsmfogbyth.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhibmFid3hiZmJ0c21mb2dieXRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4OTYzOTAsImV4cCI6MjAyNzQ3MjM5MH0.9IrXgTbQ8fGlvFSjwuth2YjefhybRZFU3bTg5mDiMzk',
+    );
+  }
 
   @Get()
-  getHello(): HttpStatus {
-    return HttpStatus.OK;
+  async getHello() {
+    const { data } = await this.supabase.from('group').select(`
+                name,
+                permission!group_permission(name)
+  `);
+    return data;
   }
 }
