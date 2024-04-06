105213cbbf5360280c50c264c08f94c62ce4d1c7
Daniel <daniilterawars@gmail.com>
2024-04-06 14:27:47 +0300

    feat(layout): auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,6 @@ import GlobalLayout from '@/app/global-layout'
 export default function LoginLayout({ children }: { children: ReactNode }) {
   return (
     <GlobalLayout>
-      <div>Auth layout</div>
       <div>{children}</div>
     </GlobalLayout>
   )
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,13 +2,18 @@ import { Input } from '@/shared/ui/input'
 
 export default function LoginPage() {
   return (
-    <div>
-      Login
-      <div className="text-white">123</div>
-      <div className="flex flex-col gap-3">
-        <Input label="Почта" />
-        <Input label="Пароль" />
-      </div>
+    <div className="flex min-h-screen">
+      <aside className="flex items-center border-r border-neutral-700 p-12">
+        <div>
+          <h2 className="text-3xl">Welcome back</h2>
+          <p>Sign in to your account</p>
+
+          <div className="flex flex-col gap-6">
+            <Input label="Почта" />
+            <Input label="Пароль" />
+          </div>
+        </div>
+      </aside>
     </div>
   )
 }
