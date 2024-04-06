9c3dc1244a3e0bd17ee30ba8e4b3a13e505dc23f
Daniel <daniilterawars@gmail.com>
2024-04-06 22:45:57 +0300

    feat(backend): supabase

--- mono/my-turborepo/apps/backend/src/app.controller.ts
+++ mono/my-turborepo/apps/backend/src/app.controller.ts
@@ -1,23 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { createClient } from '@supabase/supabase-js';
+import { Supabase } from './supabase/supabase';
 
 @Controller()
 export class AppController {
-  supabase: any;
-
-  constructor(private readonly appService: AppService) {
-    this.supabase = createClient(
-      'https://hbnabwxbfbtsmfogbyth.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhibmFid3hiZmJ0c21mb2dieXRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4OTYzOTAsImV4cCI6MjAyNzQ3MjM5MH0.9IrXgTbQ8fGlvFSjwuth2YjefhybRZFU3bTg5mDiMzk',
-    );
-  }
+  constructor(private readonly supabase: Supabase) {}
 
   @Get()
   async getHello() {
-    const { data } = await this.supabase.from('group').select(`
+    const { data } = await this.supabase.getClient().from('group').select(`
                 name,
-                permission!group_permission(name)
+                permissions:permission!group_permission(id, name)
   `);
     return data;
   }
--- mono/my-turborepo/apps/backend/src/app.module.ts
+++ mono/my-turborepo/apps/backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { SupabaseModule } from './supabase/supabase.module';
+import { APP_GUARD } from '@nestjs/core';
+import { SupabaseGuard } from './supabase/supabase.guard';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), PassportModule, SupabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: SupabaseGuard,
+    },
+  ],
 })
 export class AppModule {}
