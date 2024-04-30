c2fa6c1fca22efe2a2223c91b10683c1a31f1028
Daniel <daniilterawars@gmail.com>
2024-04-30 14:12:06 +0300

    feat: layout

--- mono/my-turborepo/apps/web/src/app/(dashboard)/page.tsx
+++ mono/my-turborepo/apps/web/src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 import { Login } from '@/app/(dashboard)/LoginBtn'
-import { getAll } from '@/entities/test/get-all'
 
 export default async function HomePage() {
   return (
@@ -7,6 +6,16 @@ export default async function HomePage() {
       <h1>Home Page</h1>
       <Login />
       {/*<pre>{JSON.stringify(data, null, 2)}</pre>*/}
+
+      <div className="mx-auto grid gap-6 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="p-6 border border-neutral-900 rounded-md bg-neutral-900">
+          <div className="p-4 bg-neutral-950 rounded-md mb-4"></div>
+          New document
+        </div>
+        <div className="p-6 border border-neutral-900 rounded-md bg-neutral-900">
+          New folder
+        </div>
+      </div>
     </div>
   )
 }
