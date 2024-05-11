c5a8e3c3e547eb3732ef8e4d9674c87a56ce5599
Daniel <daniilterawars@gmail.com>
2024-05-11 15:12:14 +0300

    feat(tree): create

--- mono/my-turborepo/packages/ui/src/tree/tree.tsx
+++ mono/my-turborepo/packages/ui/src/tree/tree.tsx
@@ -3,11 +3,13 @@
 import { useMemo, useState } from 'react'
 
 import { TreeNode } from './TreeNode'
+import { create } from './utils/create'
 import { flatten } from './utils/flatted'
 import { RawNode } from './utils/interface'
 
 export const Tree = () => {
   const [internalExpandedKeys, setInternalExpandedKeys] = useState([1])
+  create()
 
   const nodes: any[] = [
     {
@@ -15,7 +17,15 @@ export const Tree = () => {
       label: 'Node 1',
       level: 0,
       children: [
-        { key: 2, isLeaf: false, label: 'Node 1.1', level: 1 },
+        {
+          key: 2,
+          label: 'Node 1.1',
+          level: 1,
+          children: [
+            { key: 5, label: 'Node 1.1.1', level: 2 },
+            { key: 6, label: 'Node 1.1.2', level: 2 },
+          ],
+        },
         { key: 3, isLeaf: false, label: 'Node 1.2', level: 1 },
         { key: 4, isLeaf: false, label: 'Node 1.3', level: 1 },
       ],
---
+++ mono/my-turborepo/packages/ui/src/tree/utils/create.ts
@@ -0,0 +1,97 @@
+import { Key, TreeNode } from './interface'
+
+const createdData = [
+  {
+    key: '1',
+    children: [
+      {
+        key: '1.1',
+        children: [
+          {
+            key: '1.1.1',
+            children: [
+              {
+                key: '1.1.1.1',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]
+
+export type TreeNodeMap<R, G, I> = Map<Key, TreeNode<R, G, I>>
+
+export type LevelTreeNodeMap<R, G, I> = Map<number, Array<TreeNode<R, G, I>>>
+
+export type GetChildren<R, G, I> = (
+  node: R | G | I,
+) => Array<R | G | I> | unknown
+
+export function defaultGetChildren<R, G, I>(
+  node: R | G | I,
+): Array<R | G | I> | unknown {
+  return (node as any).children
+}
+
+function createTreeNodes<R, G, I>(
+  rawNodes: Array<R | G | I>,
+  treeNodeMap: TreeNodeMap<R, G, I>,
+  levelTreeNodeMap: LevelTreeNodeMap<R, G, I>,
+  getChildren: GetChildren<R, G, I>,
+  parent: TreeNode<R, G> | null = null,
+  level: number = 0,
+): Array<TreeNode<R, G, I>> {
+  const treeNodes: Array<TreeNode<R, G, I>> = []
+
+  rawNodes.forEach((rawNode, index) => {
+    const treeNode: TreeNode<R, G, I> = Object.create({})
+    treeNode.rawNode = rawNode
+    treeNode.siblings = treeNodes
+    treeNode.level = level
+    treeNode.index = index
+    treeNode.isFirstChild = index === 0
+    treeNode.isLastChild = index + 1 === rawNodes.length
+    treeNode.parent = parent
+
+    if (!treeNode.ignored) {
+      const rawChildren = getChildren(rawNode as R | G)
+      if (Array.isArray(rawChildren)) {
+        treeNode.children = createTreeNodes<R, G, I>(
+          rawChildren,
+          treeNodeMap,
+          levelTreeNodeMap,
+          getChildren,
+          treeNode as unknown as TreeNode<R, G>,
+          level + 1,
+        )
+      }
+    }
+    treeNodes.push(treeNode)
+    treeNodeMap.set(treeNode.key, treeNode)
+    if (!levelTreeNodeMap.has(level)) levelTreeNodeMap.set(level, [])
+    levelTreeNodeMap.get(level)?.push(treeNode)
+  })
+
+  return treeNodes as any
+}
+
+export const create = () => {
+  console.log('create')
+  const treeNodeMap = new Map()
+  const levelTreeNodeMap = new Map()
+
+  const rawNodes = createdData
+
+  const treeNodes = createTreeNodes(
+    rawNodes,
+    treeNodeMap,
+    levelTreeNodeMap,
+    defaultGetChildren,
+  )
+
+  console.log('TREE NODES', treeNodes)
+  console.log('treeNodeMap', treeNodeMap)
+  console.log('levelTreeNodeMap', levelTreeNodeMap)
+}
